6fe546b0d25f35469a5c947e611ad5fb3e7fd092
Hautecoeur <hautecoeurludovic@gmail.com>
2018-07-10 16:22:48 +0200

    integration chart for first tab

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,32 @@
-$('#table1').before('<div id="chartContainer">Test</div>');
 
-var svg = dimple.newSvg("#chartContainer", 590, 400);
-d3.tsv("data.csv", function (data) {
-  data = dimple.filterData(data, "Owner", ["Aperture", "Black Mesa"])
-  var myChart = new dimple.chart(svg, data);
-  myChart.setBounds(60, 30, 505, 305);
-  var x = myChart.addCategoryAxis("x", "Month");
-  x.addOrderRule("Date");
-  myChart.addMeasureAxis("y", "Unit Sales");
-  var s = myChart.addSeries("Channel", dimple.plot.line);
-  s.interpolation = "cardinal";
-  myChart.addLegend(60, 10, 500, 20, "right");
-  myChart.draw();
-});
+let pays = document.getElementById('table1').getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+console.log(pays);
+let test = pays[1].children[1];
+console.log(test);
+
+$(pays).click(function() {
+    let test = pays[1].children[1];
+    if (test === 1) {
+        console.log("1");
+    }
+    
+    
+    
+  });
+
+$('#table1').before('<div id="chart1"></div>');
+
+var svg = dimple.newSvg("#chart1", 700, 400);
+data = [
+    { "Brand":"Belgique", "Années":"2001", "Nombres":900 },
+    { "Brand":"Belgique", "Années":"2002", "Nombres":1100 },
+    { "Brand":"Belgique", "Années":"2003", "Nombres":900 },
+    { "Brand":"Belgique", "Années":"2004  ", "Nombres":900 },
+];
+
+var myChart = new dimple.chart(svg, data);
+myChart.addCategoryAxis("x", "Années");
+myChart.addMeasureAxis("y", "Nombres");
+myChart.addSeries("Brand", dimple.plot.line);
+myChart.addLegend(6, 10, 700, 30, "right");
+myChart.draw();
